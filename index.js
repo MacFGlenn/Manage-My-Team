49ed9b2165e589a68cf251769aabf82c8bd0fd4e
MFG <macfglenn@gmail.com>
2022-03-06 01:04:28 -0500

    added more inquirer prompts

--- index.js
+++ index.js
@@ -40,32 +40,65 @@ const newManager = () => {
 };
 
 const newEmployee = () => {
-  return inquirer.prompt([
-    {
-      type: "rawlist",
-      name: "role",
-      message: "Choose employee's role",
-      choices: ["Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "Enter employee's name",
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "Enter employee's id"
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "Enter employee's email"
-    },
-    {
+  return inquirer
+    .prompt([
+      {
+        type: "rawlist",
+        name: "role",
+        message: "Choose employee's role",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Enter employee's name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter employee's id",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter employee's email",
+      },
+      {
+        when: (input) => input.role === "Engineer",
         type: "input",
         name: "github",
-        message: "Enter employee's github username"
-    }
-  ]);
+        message: "Enter employee's github username",
+      },
+      {
+        when: (input) => input.role === "Intern",
+        type: "input",
+        name: "school",
+        message: "Enter employee's school",
+      },
+      {
+        type: "confirm",
+        name: "confirmNewEmployee",
+        message: "Add another employee?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      let { name, id, email, role, github, school, confirmNewEmployee } =
+        employeeData;
+      let employee;
+
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+      }
+
+      teamMembers.push(employee);
+
+      if (confirmNewEmployee) {
+        return newEmployee(teamMembers);
+      } else {
+        return teamMembers;
+      }
+    });
 };
